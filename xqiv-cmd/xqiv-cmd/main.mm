13053d2c51df5988e5da8f353ba187d6b0957267
Ondrej <ondrej.holecek@gmail.com>
2013-05-04 18:44:02 +0200

    xqiv-cmd added

---
+++ xqiv-cmd/xqiv-cmd/main.mm
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xqiv-cmd
+//
+//  Created by smrt on 5/4/13.
+//  Copyright (c) 2013 smrt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
