ff97d1543e6ca0dfb82f006d6a706b8523a7c1fb
Ondrej <ondrej.holecek@gmail.com>
2013-05-04 18:50:30 +0200

    basic cmd-line tool application

--- xqiv-cmd/xqiv-cmd/main.mm
+++ xqiv-cmd/xqiv-cmd/main.mm
@@ -8,15 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+#include <stdlib.h>
+#include <iostream>
+#include <sys/param.h>
+#include <stdlib.h>
+#include <sys/syslimits.h>
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
+        NSMutableDictionary* userInfo = [NSMutableDictionary dictionaryWithCapacity:1];
+        NSMutableDictionary* argsInfo = [NSMutableDictionary dictionaryWithCapacity:1];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        for (int i=0; i<argc; i++) {
+            char buf[NAME_MAX];
+            realpath(argv[i], buf);
+            [argsInfo setObject:[NSString stringWithUTF8String:buf]
+                         forKey:[NSString stringWithFormat:@"%d", i]];
+        }
         
+        [userInfo setObject:[NSString stringWithFormat:@"%d", argc] forKey:@"argc"];
+        [userInfo setObject:argsInfo forKey:@"args"];
+
+        NSDistributedNotificationCenter *notc = [NSDistributedNotificationCenter defaultCenter];
+        [notc postNotificationName:@"xqiv-cmd"
+                            object:nil
+                          userInfo:userInfo
+                deliverImmediately:YES];
     }
     return 0;
 }
-
