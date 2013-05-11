911093a3f58f45a74008c60807293230c3dc28dd
Ondrej <ondrej.holecek@gmail.com>
2013-05-11 22:16:03 +0200

    xqiv/ns-array-of.h

--- xqiv/ns-array-of.h
+++ xqiv/ns-array-of.h
@@ -9,6 +9,46 @@
 #ifndef xqiv_ns_array_of_h
 #define xqiv_ns_array_of_h
 
+#include "ns-array.h"
+
+namespace ns {
+    template<typename Object_t>
+    class array_of_t {
+    public:
+        array_of_t() {}
+        explicit array_of_t(NSMutableArray *anArray) : a(anArray)
+        {}
+
+        void push_back(Object_t *o) {
+            a.push_back(o);
+        }
+
+        void clear() {
+            a.clear();
+        }
+
+        Object_t * operator[](size_t idx) {
+            return a[idx];
+        }
+
+        size_t size() {
+            return a.size();
+        }
+
+        
+        void replace(size_t idx, Object_t *o) {
+            a.release(idx, o);
+        }
+
+        void remove(size_t idx) {
+            a.remove(idx);
+        }
+        
+
+    private:
+        ns::array_t a;
+    };
+}
 
 
 #endif
