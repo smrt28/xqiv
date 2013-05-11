9b221f8208425d9d73b633c786c58cb11d5d9bdc
Ondrej <ondrej.holecek@gmail.com>
2013-05-11 21:03:37 +0200

    ns-array-of

---
+++ xqiv/ns-array-of.h
@@ -0,0 +1,14 @@
+//
+//  ns-array-of.h
+//  xqiv
+//
+//  Created by smrt on 5/11/13.
+//  Copyright (c) 2013 smrt. All rights reserved.
+//
+
+#ifndef xqiv_ns_array_of_h
+#define xqiv_ns_array_of_h
+
+
+
+#endif
